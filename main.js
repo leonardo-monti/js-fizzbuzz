d29f2b719e9d4ab662e0048268931d19afbb117c
leonardo-monti <monti.leonardo22@gmail.com>
2025-12-17 16:00:16 +0100

    end

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
 
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
@@ -11,6 +10,10 @@ for (let i = 1; i <= 100; i++) {
     else if
         (i % 5 === 0) {
         console.log("Buzz");
+
+    }
+    else {
+        console.log(i);
 
     }
 
